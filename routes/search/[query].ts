9868d8331b0f5870644d1e2b0842eacde49802ad
Butt <christianjuth@Vic-Vinegar-Jr.local>
2024-01-15 23:02:12 -0500

    feat: add fuzy search

--- routes/search/[query].ts
+++ routes/search/[query].ts
@@ -1,7 +1,30 @@
 
 import data from '../../data.json'
+import Fuse from 'fuse.js'
+
+
+const fuseOptions = {
+	// isCaseSensitive: false,
+	// includeScore: false,
+	shouldSort: true,
+	// includeMatches: false,
+	// findAllMatches: false,
+	// minMatchCharLength: 1,
+	// location: 0,
+	// threshold: 0.6,
+	// distance: 100,
+	// useExtendedSearch: false,
+	// ignoreLocation: false,
+	// ignoreFieldNorm: false,
+	// fieldNormWeight: 1,
+	keys: [
+		"title",
+	]
+};
+
+const fuse = new Fuse(data, fuseOptions);
 
 export default eventHandler((event) => {
   const { query } = event.context.params
-  return data.filter(item => item.title.toLowerCase().includes(query.toLowerCase()))
+  return fuse.search(query).slice(0, 100).map(result => result.item)
 })
